24e53f79ca84926cc76d393e45a53f683fe30e4e
aold619 <aold619@gmail.com>
2018-01-11 10:49:16 +0800

    finished basic layout

--- app/src/main/java/com/aold619/shawn/filmhelloworld/MainActivity.java
+++ app/src/main/java/com/aold619/shawn/filmhelloworld/MainActivity.java
@@ -2,12 +2,34 @@ package com.aold619.shawn.filmhelloworld;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button posterButton;
+    private Button trailerButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        posterButton = findViewById(R.id.bt_poster);
+        trailerButton = findViewById(R.id.bt_trailer);
+
+        posterButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        trailerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
+
 }
